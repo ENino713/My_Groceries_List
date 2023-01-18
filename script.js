8f2defdc3c868244693274d388eaf15f6b146f53
ENino713 <enino7@stu.southtexascollege.edu>
2023-01-18 14:53:04 -0600

    Save grocery items

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import { LocalDB } from 'https://cdn.skypack.dev/peadb'
+import shortid from 'https://cdn.skypack.dev/shortid'
+
 const db = new LocalDB('grocery-list-db') 
-const groceries = db.getAll ||  []
+const groceries = db.getAll() || []
 
 const groceryList = document.getElementById('groceryList')
 const newGroceryinput = document.getElementById('newGrocery')
@@ -8,7 +10,7 @@ const addBtn = document.getElementById('addBtn')
 
 const createGroceryElement = grocery => {
   const groceryElement = document.createElement('li')
-    groceryElement.innerText = grocery
+    groceryElement.innerText = grocery.value
     groceryElement.classList.add('groceryItem')
     return groceryElement
 } 
@@ -20,8 +22,10 @@ const addGrocery = newGrocery => {
 addBtn.addEventListener('click', e => {
     e.preventDefault()
     const value  = newGroceryinput.value
+    const key = shortid.generate()
     if (value) {
-        addGrocery(value)
+        addGrocery({key, value})
+        db.set ({key, value}) 
         newGroceryinput.value = null
     }
 })
